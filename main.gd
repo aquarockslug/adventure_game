0e2e13eb483b9166e831c252fd5ae61f038c9942
aquarock <aqua.rock.slug@gmail.com>
2024-01-29 15:55:07 -0600

    updated scene_text

--- main.gd
+++ main.gd
@@ -3,90 +3,53 @@ extends Node2D
 var scene_text = {
 	"phone.tscn": {
 		"dialogue": [ 
-			"you have just recieved a text message from your mother",
-			"she has a date for you"
+			"You arrive in room 1..",
+			"There is..."
 		],
-		"question": "The date starts in an hour... What will you do?",
+		"question": "What will you do?",
 		"options": [
-			{"text": "Take a long bath", "affection":5, "next": "drive.tscn"},
-			{"text": "Take a one hour nap", "affection":1, "next": "drive.tscn"},
-			{"text": "Roll around in mud", "affection":-5, "next": "drive.tscn"},
-			{"text": "Rush out the door", "affection":1, "next": "drive.tscn"}
+			{"text": "go to 2", "affection":5, "next": "drive.tscn"},
+			{"text": "go to 2", "affection":1, "next": "drive.tscn"},
+			{"text": "go to 3", "affection":-5, "next": "distraction.tscn"},
+			{"text": "go to 3", "affection":1, "next": "distraction.tscn"}
 		]
 	},
 
 	"drive.tscn": {
 		"dialogue": [ 
-			"You are driving down the street"
+			"You leave room 1 and arrive at room 2..."
 		],
-		"question": "You see a flower shop. Stop?",
-		"options": [# todo: allow only two options
-			{"text": "Get some flowers", "affection":0, "next": "distraction.tscn"},
-			{"text": "Be on time", "affection":0, "next": "distraction.tscn"},
-			{"text": "Get some flowers", "affection":0, "next": "distraction.tscn"},
-			{"text": "Be on time", "affection":0, "next": "distraction.tscn"}
+		"question": "What will you do",
+		"options": [
+			{"text": "1", "affection":0, "next": ""},
+			{"text": "2", "affection":0, "next": ""},
+			{"text": "3", "affection":0, "next": ""},
+			{"text": "4", "affection":0, "next": ""}
 		]
 	},
 
 	"distraction.tscn": {
 		"dialogue": [ 
-			"You have arrived at the coffee shop",
-			"Your date asks a personal question"
+			"You leave room 1 and arrive at room 3...",
+			"There is..."
 		],
 		"question": "How can you get out of this situation?",
 		"options": [
-			{"text": "ignore her and change the subject", "affection":0, "next": "neutral.tscn"},
-			{"text": "Pretend to fall off your chair", "affection":0, "next": "fire.tscn"},
-			{"text": "Excuse yourself and go to the bathroom", "affection":0, "next": "fire.tscn"},
-			{"text": "Accuse her of a crime", "affection":0, "next": "argument.tscn"}
-		]
-	},
-	
-	"fire.tscn": {
-		"dialogue": [ ],
-		"question": "Its getting hot in here, what do you do?",
-		"options": [
-			{"text": "Try to put out the fire", "affection":0, "next": "jail.tscn"},
-			{"text": "Run away", "affection":0, "next": "good_end.tscn"},
-			{"text": "Panic", "affection":0, "next": "jail.tscn"},
-			{"text": "Blame your date", "affection":0, "next": "jail.tscn"}
-		]
-	},
-	
-	"neutral.tscn": {
-		"dialogue": [ ],
-		"question": "You managed to move on, but now she is asking for another date?",
-		"options": [
-			{"text": "Agree to a second date", "affection":0, "next": "bad_end.tscn"},
-			{"text": "Run away", "affection":0, "next": "bad_end.tscn"},
-			{"text": "Panic", "affection":0, "next": "good_end.tscn"},
-			{"text": "Propose marrige", "affection":0, "next": "bad_end.tscn"}
-		]
-	},
-	
-	"argument.tscn": {
-		"dialogue": [ ],
-		"question": "The whole coffee shop is looking at you, what do you do?",
-		"options": [
-			{"text": "try to calm everyone down", "affection":0, "next": "good_end.tscn"},
-			{"text": "run away", "affection":0, "next": "good_end.tscn"},
-			{"text": "panic", "affection":0, "next": "good_end.tscn"},
-			{"text": "blame your date", "affection":0, "next": "bad_end.tscn"}
+			{"text": "1", "affection":0, "next": ""},
+			{"text": "2", "affection":0, "next": ""},
+			{"text": "3", "affection":0, "next": ""},
+			{"text": "4", "affection":0, "next": ""}
 		]
 	}
 }
 
 var current_scene
 var affection = 50
-#if var == 0, adv to bad ending; if var ==100, adv to elope ending immediately
-#else keep playing; once '20 questions' is over, score checked 25 < aff <75 correlates to bad < good < elope
 
 var scene_tscn = {
 	"phone.tscn": preload("res://phone.tscn"),
 	"drive.tscn": preload("res://drive.tscn"),
 	"distraction.tscn": preload("res://distraction.tscn"),
-	"fire.tscn": preload("res://fire.tscn"),
-	"jail.tscn": preload("res://jail.tscn"),
 }
 
 func _ready():
@@ -109,8 +72,10 @@ func play_scene(scene_name):
 	if scene_name in scene_tscn:
 		current_scene = scene_tscn[scene_name].instantiate()
 		self.add_child(current_scene)
-
-	$dialogue.play(scene_text[scene_name])
+	
+	# play dialogue
+	if scene_text[scene_name]:
+		$dialogue.play(scene_text[scene_name])
 	
 func open_prompt(scene):
 	$prompt.visible = true
